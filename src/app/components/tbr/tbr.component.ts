5882cb2ae981a596cd160298debf4530cf1f62e0
Juho Marila <juho.marila@student.lab.fi>
2022-10-06 20:16:21 +0300

    tobered component init

--- src/app/components/tbr/tbr.component.ts
+++ src/app/components/tbr/tbr.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/shared/interfaces/article';
+import { Favourite } from 'src/app/shared/interfaces/favourite';
+import { FavouriteService } from 'src/app/shared/services/favourite.service';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
 
 @Component({
   selector: 'app-tbr',
   templateUrl: './tbr.component.html',
 })
 export class TbrComponent implements OnInit {
-  constructor() {}
+  favouriteList: Favourite[] = [];
+  favouriteArticlesList: Article[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private favouriteSvc: FavouriteService,
+    private firestoreSvc: FirestoreService
+  ) {}
+
+  ngOnInit(): void {
+    this.getFavouriteArticles();
+    this.favouriteList?.map(key => {
+      this.firestoreSvc.getArticle(key.key).subscribe(article => {
+        this.favouriteArticlesList.push(article.data() as Article);
+      });
+    });
+  }
+
+  getFavouriteArticles(): void {
+    this.favouriteSvc
+      .getUserArticleFavourites()
+      .subscribe(favs => (this.favouriteList = favs));
+  }
 }
